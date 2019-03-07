09058929688399518ed8c490f54a4caf81531c36
Sankalp100 <sankalpgour123@gmail.com>
2019-03-07 19:08:55 +0530

    simple node example

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect = {
+    perameter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = " + l + "and b = " + b);
+
+    if ( l <= 0 || b<=0){
+        console.log("Rectangle dimension should be greater then zero: l = " + l + "and b = " + b);
+    }
+    else {
+        console.log("the area of rectangle is "+ rect.area(l,b));
+        console.log("The perameter of rectangle is " + rect.perameter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
